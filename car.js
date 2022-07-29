57a5e550ff9ee24fc7b8caf97949e657ac33afca
Amr_MAM <amr.mostafa.abbas@gmail.com>
2022-07-29 15:09:38 +0200

    29Jul2022_00

--- car.js
+++ car.js
@@ -4,16 +4,76 @@ class Car {
         this.y = y;
         this.width = width;
         this.height = height;
+
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+
+        this.angle = 0;
+
+        this.controls = new controls();
     }
 
+    update() {
+        this.#move();
+    }
+
+    #move(){
+        if (this.controls.forward == true) {
+            this.speed += this.acceleration;
+        }
+        if (this.controls.reverse == true) {
+            this.speed -= this.acceleration;
+        }
+
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        if (this.speed < -this.maxSpeed) {
+            this.speed = -this.maxSpeed;
+        }
+
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        }
+        if (this.speed < 0) {
+            this.speed += this.friction; 
+        }
+
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+
+        if (this.speed != 0) {
+            const flip = this.speed < 0 ? -1 : 1;
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
+            }
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
+            }
+
+        }
+
+
+        this.y -= this.speed * Math.cos(this.angle);
+        this.x -= this.speed * Math.sin(this.angle);
+
+    }
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            - this.width / 2,
+            -this.height / 2,
             this.width,
             this.height,
         );
         ctx.fill()
+        ctx.restore();
     }
 }
